296edf13e645b348530fd34f61a5813004b83417
Adil Khan <adil.khan@outlook.com>
2017-02-08 20:52:03 -0500

    Passes callback to external component - much better than passing reference to the whole app

--- components/MyComponent.js
+++ components/MyComponent.js
@@ -1,13 +1,12 @@
 import { ListView } from 'react-native';
 export default class MyComponent {
-  static someWords(app) {
-    return fetch('http://localhost:3001/api/names')
+  static someWords(callback) {
+    return fetch('http://192.168.1.159:3001/api/names')
       .then(function(response) {
         return response.json();
       }).then(function(value){
         console.log("setting app.names " + value.names );
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        app.setState({namesSource: ds.cloneWithRows(value.names)});
+        callback(value)
       });
   }
 
--- index.android.js
+++ index.android.js
@@ -3,6 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -18,20 +19,35 @@ import MyComponent from './components/MyComponent.js';
 export default class HelloReactNative extends Component {
   constructor(){
     super();
+    this.names = ['a','b'];
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       dataSource: ds.cloneWithRows(MyComponent.arrayOfSentence(1)),
+      namesSource: ds.cloneWithRows(['foo','bar'])
     };
+    this.apiCallback = this.apiCallback.bind(this);
+    MyComponent.someWords(this.apiCallback);
   }
+
+  apiCallback(value){
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.setState({namesSource: ds.cloneWithRows(value.names)});
+  }
+
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
+    console.log('state: ' + this.state.names);
     return (
       <View>
         <ListView
           dataSource={this.state.dataSource}
           renderRow={() => <Image source={pic} style={{width: 193, height: 110}} />}
+        />
+        <ListView 
+          dataSource={this.state.namesSource}
+          renderRow={(dataRow) => <Text>{dataRow} </Text>}
         />
       </View>
     );
